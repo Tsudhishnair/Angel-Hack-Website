20a3a3221e26451a6c2dcb7a9f66d62a84fab8da
Prejith Premkumar <prajithpremg@gmail.com>
2018-07-29 07:04:04 +0530

    merge

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
+$(function () {
+  $('.modal-view').click(function() {
+    $('.modal').addClass('is-active')
+  })
 
-let cli = document.querySelectorAll(".modal-view");
-let clicklist = cli.length;
-function modalview() {
-  for (let i = 0; i < clicklist; i++) {
-    cli = addEventListener("click",function(){
-      document.querySelector(".modal").classList.add("is-active");
-    });
-    
-    document.querySelector(".modal-close-btn").classList.remove("is-active");  
-}
-}
-window.addEventListener("load",modalview(),false);
+  $('.modal-close-btn').click(function() {
+    $('.modal').removeClass('is-active')
+  })
+
+  var shopRef = firebase.database().ref('/Shops/1')
+  var orderRef = firebase.database().ref('/orders')
+});
