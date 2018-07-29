da9fe006ae4b5706267eaaa3fbbd44bca3658e24
Prejith Premkumar <prajithpremg@gmail.com>
2018-07-29 07:37:24 +0530

    firebase

--- app.js
+++ app.js
@@ -7,17 +7,13 @@ $(function () {
     $('.modal').removeClass('is-active')
   })
 
-  var shopRef = firebase.databclearase().ref('/Shops/1')
+  var shopRef = firebase.database().ref('/Shops/1')
   var orderRef = firebase.database().ref('/orders')
 
   var $itemTable = $('#shop-items');
+
+  var $item = orderRef.once('value').then( snapshot => {
+    console.log(snapshot.val())
+  })
   $itemTable.prepend(`<tr><td>gthtfh</td><td>hft</td><td>hft</td><td>hft</td></tr>`)
 });
-
-var data = [{
-  x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
-  y: [1, 3, 6],
-  type: 'scatter'
-}];
-
-Plotly.newPlot('myDiv', data);
