80311bbcbd6d4b4b04175cfe44ef6eb1ba15395c
Prejith Premkumar <prajithpremg@gmail.com>
2018-07-29 08:52:55 +0530

    firebase listener for modal

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 $(function () {
-  $('.modal-view').click(function () {
-    $('.modal').addClass('is-active')
-  })
-
-  $('.modal-close-btn').click(function () {
-    $('.modal').removeClass('is-active')
-  })
-
   var shopRef = firebase.database().ref('/Shops/1')
   var orderRef = firebase.database().ref('/orders')
+  var productRef = firebase.database().ref('/Products/')
 
   var $itemTable = $('#shop-items');
 
-  var $item = orderRef.once('value').then( snapshot => {
-    console.log(snapshot.val())
+  const now = new Date();
+
+  function mapStatus(num) {
+    if (num === 1) {
+      return "Order Arrived"
+    } else if (num === 2) {
+      return "Packing"
+    } else if (num === 3) {
+      return "Bag Packed"
+    }
+  }
+
+  orderRef.once('value').then(snapshot => {
+    var val = snapshot.val();
+
+    var items = snapshot.val().products
+    let count = 0;
+    for (key in items) {
+      count += Number(items[key])
+    }
+
+    $itemTable.prepend(`<tr class="modal-view"><td>Pickup</td><td>${now.toLocaleTimeString()}</td><td>${count}</td><td>${mapStatus(val.status)}</td></tr>`)
+
+    $('.modal-view').click(function () {
+      $('.modal').addClass('is-active')
+    })
+
+    $('.modal-close-btn').click(function () {
+      $('.modal').removeClass('is-active')
+    })
+
+    for (key in items) {
+      var itemRef = productRef.child('/key')
+      $('#items-shop').append(`<tr><td>${key}</td><td>${itemRef.name}</td><td>${items[key]}</td><td>${items[key] * itemRef.offer_price}</td></tr>`)
+    }
   })
-  $itemTable.prepend(`<tr><td>gthtfh</td><td>hft</td><td>hft</td><td>hft</td></tr>`)
 });
